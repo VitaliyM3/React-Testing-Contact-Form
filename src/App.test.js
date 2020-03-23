04bd4f97710682359bf06269f4805634da0f482b
Vitaliy Mikhaylyyk <vitaliymikhaylyuk@gmail.com>
2020-03-22 20:50:42 -0400

    2 tests written the way kirby described

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,29 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+// test("blank firstname blur displays an error", async ()=>{
+//   const container = render(<ContactForm />);
+//   const firstNameField = container.getByTestId("firstName");
+//   act(()=> {
+//     fireEvent.blur(firstNameField);
+//   })
+//   const error = await waitForElement(()=>container.getByTestId("firstNameError"));
+// })
+
+//1. 
+test("firstname renders correctly ", () => {
+  render(<label htmlFor="firstName">First Name*</label>);
+});
+
+//2.
+test("div APP renders correctly", ()=> {
+  render(<div className="App"></div>);
+});
+
+//3.
+
+
+//4.
+
+
